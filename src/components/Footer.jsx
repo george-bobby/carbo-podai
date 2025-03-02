5a2214ed3b7a12615b74e42b13c901d2fcf2cb97
George Bobby <georgebobbyawesome@gmail.com>
2025-03-02 16:41:08 +0530

    fix again

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -62,7 +62,7 @@ export default function Footer() {
 								},
 								{
 									icon: <FaLeaf className='text-emerald-400' />,
-									label: 'Electic Appliances',
+									label: 'Electric Appliances',
 									href: '/ecocenter',
 								},
 								{
